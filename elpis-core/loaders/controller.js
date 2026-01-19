01a878e33bb5e73622b2e1d39440520a70872659
guanwei <guanw01@mingyuanyun.com>
2026-01-19 15:44:43 +0800

    elpis引擎--controller,service

--- elpis-core/loaders/controller.js
+++ elpis-core/loaders/controller.js
@@ -1,1 +1,48 @@
-module.exports = (app) => {};
+const path = require('path');
+const glob = require('glob');
+
+const { sep } = path;
+
+/**
+ * controller loader
+ * @param {*} app koa 实例
+ *
+ * 加载所有 controller，使得可通过 'app.controller.${目录}.${文件}' 访问
+ */
+module.exports = (app) => {
+  const controllerPath = path.resolve(app.businessPath, `.${sep}controller`);
+
+  const fileList = glob.sync(path.resolve(controllerPath, `.${sep}**${sep}*.js`));
+
+  const controllers = {};
+
+  fileList.forEach((file) => {
+    // 获取文件名：app/controller/test.js --> test
+    //           app/controller/custom/test.js --> custom/test
+    let name = file.replace(controllerPath + sep, '').replace('.js', '');
+
+    // 转换为驼峰式命名：custom/test-controller --> custom/testController
+    name = name.replace(/[-_](\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
+
+    let tempController = controllers;
+
+    const nameList = name.split(sep);
+
+    // 实现：{ test: [Function], custom: { customController: [Function] } }
+    // 使得可以：custom.customController 这样进行使用
+    nameList.forEach((item, index) => {
+      if (index === nameList.length - 1) {
+        const ControllerModule = require(file)(app);
+        tempMiddleware[item] = new ControllerModule();
+      } else {
+        if (!tempController[item]) {
+          tempController[item] = {};
+        }
+
+        tempController = tempController[item];
+      }
+    });
+  });
+
+  app.controllers = controllers;
+};
--- elpis-core/loaders/service.js
+++ elpis-core/loaders/service.js
@@ -1,1 +1,48 @@
-module.exports = (app) => {};
+const path = require('path');
+const glob = require('glob');
+
+const { sep } = path;
+
+/**
+ * service loader
+ * @param {*} app koa 实例
+ *
+ * 加载所有 service，使得可通过 'app.service.${目录}.${文件}' 访问
+ */
+module.exports = (app) => {
+  const servicePath = path.resolve(app.businessPath, `.${sep}service`);
+
+  const fileList = glob.sync(path.resolve(servicePath, `.${sep}**${sep}*.js`));
+
+  const services = {};
+
+  fileList.forEach((file) => {
+    // 获取文件名：app/service/test.js --> test
+    //           app/service/custom/test.js --> custom/test
+    let name = file.replace(servicePath + sep, '').replace('.js', '');
+
+    // 转换为驼峰式命名：custom/test-service --> custom/testService
+    name = name.replace(/[-_](\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
+
+    let tempService = services;
+
+    const nameList = name.split(sep);
+
+    // 实现：{ test: [Function], custom: { customService: [Function] } }
+    // 使得可以：custom.customService 这样进行使用
+    nameList.forEach((item, index) => {
+      if (index === nameList.length - 1) {
+        const ServiceModule = require(file)(app);
+        tempService[item] = new ServiceModule();
+      } else {
+        if (!tempService[item]) {
+          tempService[item] = {};
+        }
+
+        tempService = tempService[item];
+      }
+    });
+  });
+
+  app.services = services;
+};
