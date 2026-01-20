f84866ce1fb0fb2faaf682aee05aee36441fbb87
guanwei <guanw01@mingyuanyun.com>
2026-01-20 23:39:16 +0800

    全局错误中间件

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -9,6 +9,27 @@ module.exports = (app) => {
       // 因为，在后续所有的操作，都会放在 next() 之后执行
       // 那么就会捕捉到所有后续操作的异常
       await next();
-    } catch (error) {}
+    } catch (error) {
+      const { status, message, detail } = error;
+      app.logger.info(JSON.stringify(error));
+      app.logger.error('[-- exception --]:', error);
+      app.logger.error('[-- exception --]:', status, message, detail);
+
+      if (message && message.includes('template not found')) {
+        // 访问到不存在的页面，进行重定向
+        ctx.status = 302;
+        ctx.redirect(app.options?.homePage || '/');
+        return;
+      }
+
+      const resBody = {
+        success: false,
+        code: 50000,
+        message: '请求错误，请稍后重试',
+      };
+
+      ctx.status = 200;
+      ctx.body = resBody;
+    }
   };
 };
