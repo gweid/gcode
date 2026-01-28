d5e55f8283d88e1ebe435d10ea1f278119ed1260
guanwei <guanw01@mingyuanyun.com>
2026-01-28 23:23:06 +0800

    webpack基础配置---splitChunk分包

--- app/pages/page2/page2.vue
+++ app/pages/page2/page2.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue';
+import { addTag } from '@utils/tools';
+
 const content = ref('');
 </script>
 
@@ -7,7 +9,7 @@ const content = ref('');
   <div class="wrapper">
     <h1>Page 2</h1>
     <input v-model="content" />
-    <p>{{ content }}</p>
+    <p>{{ content ? addTag(content) : '' }}</p>
   </div>
 </template>
 
--- app/webpack/config/webpack.base.js
+++ app/webpack/config/webpack.base.js
@@ -108,7 +108,39 @@ const baseConfig = {
       ],
     }),
   ],
-  optimization: {},
+  optimization: {
+    /**
+     * 把 js 文件打包成三种类型
+     * 1. vendor: 第三方 lib 库，基本不会改动，除非依赖版本升级
+     * 2. common: 业务组件代码的公共部分抽取出来，改动较少
+     * 3. entry.{page}: 不同页面 entry 里的业务组件代码的差异部分，会经常改动
+     * 目的：把改动和引用频率不一样的 js 区分出来，达到更好利用浏览器缓存的效果
+     */
+    splitChunks: {
+      chunks: 'all', // 对所有类型的块进行代码拆分（包括同步和异步块）
+      // maxAsyncRequests: 如果某个文件异步引入了 20 个 chunk，webpack 打包时会合并成 10 个，目的是优化浏览器的并发请求数
+      maxAsyncRequests: 10, // 每次异步加载的最大并行请求数，如import()、路由懒加载等.
+      maxInitialRequests: 10, // 入口点的最大并行请求数
+      minSize: 20000, // 模块大于 20KB 才进行代码拆分
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors', // 打包后的文件名
+          priority: 20, // 优先级，数字越大，优先级越高，node_modules 的优先级要比 common 高，保证第三方库不会被归为 common
+          enforce: true, // 强制执行
+          reuseExistingChunk: true, // 复用已有的公共 chunk
+        },
+        common: {
+          test: /[\\/]components|utils[\\/]/, // 匹配到 components、utils 目录下的模块
+          name: 'common',
+          minChunks: 2, // 模块至少被两个不同的入口引用才会被打包到 common chunk 中
+          priority: 10,
+          enforce: true,
+          reuseExistingChunk: true, // 复用已有的公共 chunk
+        },
+      },
+    },
+  },
 };
 
 module.exports = baseConfig;
